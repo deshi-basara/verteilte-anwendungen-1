6fa2980b88ff0fede098971c366c6303f3ddac9e
deshi-basara <simon@i-was-perfect.net>
2015-06-18 14:42:37 +0200

    Create registry entry on servlet-context initiation

--- 9.1 RMI/src/main/java/hfu/simon/listener/SaleListener.java
+++ 9.1 RMI/src/main/java/hfu/simon/listener/SaleListener.java
@@ -6,6 +6,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 /**
  * Created by simon on 22.04.15.
@@ -13,8 +19,10 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class SaleListener implements ServletContextListener {
 
-     @Override
-     public void contextInitialized(ServletContextEvent event) {
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+         final int REGISTRY_PORT = 1099;
+
          // get init-parameters
          ServletContext sc = event.getServletContext();
 
@@ -26,12 +34,30 @@ public class SaleListener implements ServletContextListener {
          String databasePassword = sc.getInitParameter("database-password");
          boolean useHeroku = Boolean.valueOf(sc.getInitParameter("use-heroku"));
 
-         // create a database connection
-         SaleDB saleDBModel = new SaleDB(ticketsAvailable, databaseHost, databasePort,
-                 databaseName, databaseUser, databasePassword, useHeroku);
-         sc.setAttribute("saleModel", saleDBModel);
+         // create a database connection and remote-object
+        SaleDB saleDBModel;
+         try {
+             saleDBModel = new SaleDB(ticketsAvailable, databaseHost, databasePort,
+                     databaseName, databaseUser, databasePassword, useHeroku);
+             sc.setAttribute("saleModel", saleDBModel);
+         } catch(RemoteException e) {
+             e.printStackTrace();
 
-     }
+             return;
+         }
+
+        // register the remote-object
+        try {
+            LocateRegistry.createRegistry(REGISTRY_PORT);
+
+            Registry registry = LocateRegistry.getRegistry(REGISTRY_PORT);
+            registry.rebind("saleModel", saleDBModel);
+        } catch(RemoteException e) {
+            e.printStackTrace();
+
+            System.exit(1);
+        }
+    }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
